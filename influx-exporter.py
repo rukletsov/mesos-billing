f8bcb1d1f3ded82f16fc3035303687b4497f8de7
Alexander Rukletsov <rukletsov@gmail.com>
2015-07-31 12:14:01 +0200

    Influx exproter supports cpus synchronously.

--- influx-exporter.py
+++ influx-exporter.py
@@ -3,24 +3,28 @@ import signal
 import sys
 import time
 import datetime
+import requests
+import json
 from threading import Thread
 
 from influxdb import InfluxDBClient
 
 
-# Global constants.
+# Globals.
 shutting_down = False
+last_cpus_value = 0
 
 # Mesos constants.
+mesos_endpoint = 'http://172.18.6.177:5050/accounting'
 
 # InfluxDB constants.
 user = 'user'
 password = 'user'
-dbname = 'test1'
+dbname = 'demo1'
 dbuser = 'dbuser'
 dbuser_password = 'dbuser'
-host = 'localhost'
-port = '8086'
+influx_host = 'localhost'
+influx_port = '8086'
 
 
 def shutdown(signal, frame):
@@ -30,6 +34,35 @@ def shutdown(signal, frame):
     shutting_down = True
 
 
+def fetch_mesos():
+    r = requests.get(mesos_endpoint)
+    data = r.json()
+    return data['accounting']
+
+
+
+def process_accounting_data(data):
+    global last_cpus_value
+
+    # Sort according to the timestamp (should not be necessary in future versions)
+    data.sort(key=lambda x: x['timestamp'])
+
+    for event in data:
+        # For now drop is not cpus.
+        if event['resource']['cpus'] == 0:
+            continue
+
+        if event['what'] == "TASK_STARTED":
+            new_value = last_cpus_value + event['resource']['cpus']
+        elif event['what'] == "TASK_TERMINATED":
+            new_value = last_cpus_value - event['resource']['cpus']
+
+        last_cpus_value = new_value
+        print "STORE {} {}: {} [{}]" \
+            .format('cpus', event['timestamp'], new_value, event['framework_id'])
+        put_datapoint(event['timestamp'], 'cpus', event['framework_id'], new_value)
+
+
 def fetch_infinitely():
     global shutting_down
 
@@ -37,43 +70,27 @@ def fetch_infinitely():
         if shutting_down:
             break
 
+        data = fetch_mesos()
+        process_accounting_data(data)
         time.sleep(1)
-        print "Putting datapoint"
-        put_datapoint()
 
 
 # Puts a datapoint into the InfluxDB
-# The JSON format is
-#     json_body = [
-#         {
-#             "measurement": "cpus",
-#             "tags": {
-#                 "id": "uuid1",
-#                 "frameworkId": "2015-...",
-#                 "what": "TASK_STARTED"
-#             },
-#             "time": datetime.datetime.utcnow(),
-#             "fields": {
-#                 "value": 0.64
-#             }
-#         }
-#     ]
-def put_datapoint():
+def put_datapoint(timestamp, measurement, framework_id, value):
     json_body = [
         {
-            "measurement": "cpus",
+            "measurement": measurement,
             "tags": {
-                "id": "uuid1",
-                "frameworkId": "2015-...",
-                "what": "TASK_STARTED"
+                "framework_id": framework_id
             },
-            "time": datetime.datetime.utcnow(),
+            "time": int(timestamp),
+#            "time": datetime.datetime.utcnow(),
             "fields": {
-                "value": 0.64
+                "value": float(value)
             }
         }
     ]
-    client.write_points(json_body)
+    client.write_points(json_body, time_precision='s')
 
 
 #
@@ -84,20 +101,23 @@ if __name__ == "__main__":
     print "(Listening for Ctrl-C)"
     signal.signal(signal.SIGINT, shutdown)
 
-    client = InfluxDBClient(host, port, user, password, dbname)
+    client = InfluxDBClient(influx_host, influx_port, user, password, dbname)
 
     print("Create database: " + dbname)
     client.drop_database(dbname)
     client.create_database(dbname)
 
-    fetch_thread = Thread(target = fetch_infinitely, args = ())
-    fetch_thread.start()
+    data = fetch_mesos()
+    process_accounting_data(data)
 
-    while fetch_thread.is_alive():
-        time.sleep(1)
+#    fetch_thread = Thread(target = fetch_infinitely, args = ())
+#    fetch_thread.start()
 
-    print("Drop database: " + dbname)
-    #client.drop_database(dbname)
+#    while fetch_thread.is_alive():
+#        time.sleep(1)
+
+#    print("Drop database: " + dbname)
+#    client.drop_database(dbname)
 
     print "Goodbye!"
     sys.exit(0)
